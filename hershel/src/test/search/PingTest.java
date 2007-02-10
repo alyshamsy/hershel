7de8ace2642a045fc942168356a230d5a4440cef
m.siniavine <m.siniavine@97fcad70-2129-0410-99b9-9d13f1ff0228>
2007-02-10 16:25:36 +0000

    Initial import.

---
+++ hershel/src/test/search/PingTest.java
@@ -0,0 +1,16 @@
+package test.search;
+
+import com.search.SearchId;
+
+public class PingTest
+{
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args)
+    {
+       System.out.println(SearchId.getRandomId());
+    }
+
+}
