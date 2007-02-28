1f515f5b29f31d71979092ee20edd6b2b22caad5
m.siniavine <m.siniavine@97fcad70-2129-0410-99b9-9d13f1ff0228>
2007-02-28 01:28:47 +0000

    Separated network part from message processing parts in SearchClient

--- trunk/hershel/src/com/search/MessageHandler.java
+++ trunk/hershel/src/com/search/MessageHandler.java
@@ -1,18 +1,37 @@
 package com.search;
 
-public class MessageHandler
+import java.io.IOException;
+import java.net.InetAddress;
+
+public class MessageHandler implements PingCommunicator
 {
     private SearchId myId;
+    private RoutingTable table;
+    private SearchClient client;
     
-    public MessageHandler(SearchId myId)
+    public MessageHandler(SearchId myId, SearchClient client)
     {
         this.myId = myId;
+        table = new RoutingTable(myId, 20, new DefaultPinger(this));  
+        this.client = client;
+    }    
+    
+    public void ping(NodeState targetNode)
+    {
+        // TODO Auto-generated method stub
+        
     }
-    public SearchMessage respondTo(SearchMessage pingMessage)
+    
+    public void respondTo(SearchMessage request, InetAddress address, int port) throws IOException
     {
         SearchMessage response = new SearchMessage("ping");
         response.arguments().put("id", myId.toString());
-        return response;
+       
+        NodeState node = new NodeState(request.arguments().get("id"),
+                				address,
+                				port);
+        table.addNode(node);
+        client.sendMessage(response, node);       
     }
 
 }
