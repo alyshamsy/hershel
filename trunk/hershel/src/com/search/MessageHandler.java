1e8f7843b818b97ee8a32a2217e37c419b3cd065
jason.bullers <jason.bullers@97fcad70-2129-0410-99b9-9d13f1ff0228>
2007-03-02 05:33:14 +0000

    Added functionality for find_node messages.

--- trunk/hershel/src/com/search/MessageHandler.java
+++ trunk/hershel/src/com/search/MessageHandler.java
@@ -2,6 +2,7 @@ package com.search;
 
 import java.io.IOException;
 import java.net.InetAddress;
+import java.util.List;
 import java.util.Map;
 
 
@@ -31,7 +32,22 @@ public class MessageHandler implements PingCommunicator
         ping.arguments().put("id", myId.toString());
         return ping;
     }
-    
+
+    public void findNode(NodeState targetNode, SearchId targetId) throws IOException
+    {
+    	SearchMessage find = new SearchMessage("find_node");
+    	find.arguments().put("target", targetId.toString());
+    	find.arguments().put("id", myId.toString());
+    	client.sendMessage(find, targetNode);
+    }
+
+    private SearchMessage packageNodeList(List nodes)
+    {
+    	SearchMessage nodeList = new SearchMessage("node_list");
+    	nodeList.arguments().put("nodes", nodes.toString());
+    	return nodeList;
+    }
+
     public void respondTo(SearchMessage request, InetAddress address, int port)
     {
         NodeState node = new NodeState(request.arguments().get("id"),
@@ -46,7 +62,32 @@ public class MessageHandler implements PingCommunicator
             try
             {
                 table.addNode(node);
-                client.sendMessage(pingMessage(), node); 
+                String command = request.getCommand();
+                if (command.equals("ping"))
+                {
+                	client.sendMessage(pingMessage(), node);
+                }
+                else if (command.equals("find_node"))
+                {
+                	Map<String, String> args = request.arguments();
+                	String target = args.get("target");
+                	List nodes = table.findNode(new SearchId(target));
+                	client.sendMessage(packageNodeList(nodes), node);
+                }
+                else if (command.equals("node_list"))
+                {
+                	String nodeList = request.arguments().get("nodes");
+                	nodeList = nodeList.replaceAll("[\\[|,\\]]", "");
+                	String[] nodes = nodeList.split(", ");
+                	for (String s : nodes)
+                	{
+                		String[] args = s.split(";");
+                		NodeState n = new NodeState(args[0],
+                				InetAddress.getByName(args[1]),
+                				Integer.parseInt(args[2]));
+                		table.addNode(n);
+                	}
+                }
             }
             catch (IOException e)
             {
