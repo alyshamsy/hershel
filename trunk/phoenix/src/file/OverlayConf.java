01bf397ef9a48ce2c98f59e54adf06d55c5cc27e
conz27 <conz27@97fcad70-2129-0410-99b9-9d13f1ff0228>
2007-03-03 17:48:35 +0000

    Small restructure in the directory.
    All testcode placed in the testcode package.
    All source code - classes and functionality remain in file package

--- trunk/phoenix/src/file/OverlayConf.java
+++ trunk/phoenix/src/file/OverlayConf.java
@@ -1,9 +1,9 @@
 /* This is a parser for the File Overlay's Configuration File 
  * 
  * Configuration Variables will be in the following format:
- * 			<Variable> : <Value>
+ * 			<Key> : <Value>
  * 
- *	Variable:	must contain no spaces
+ *	Key	 :		must contain no spaces
  *  Value:		must contain no spaces
  */
 package file;
@@ -12,66 +12,5 @@ import java.io.*;
 
 public class OverlayConf {
 	
-	public String Extract( String str ) {
-		int beg = 0, end = 0 ;
-		String extract;
-		
-		return str;
-	}
-	
-	public int ReadAndParse( FileInputStream  fstream, String []var, int []value, final int MAX ){
-	
-		String strLine;
-		boolean eof = false;
-		
-		// Redirecting the FileInputStream to BufferStream
-		DataInputStream in = new DataInputStream(fstream);
-		BufferedReader buf = new BufferedReader( new InputStreamReader(in) );
-
-		// Read the first line of the config file
-		try{
-			strLine = buf.readLine();	
-			
-			if (strLine == null)
-				eof=true;
-		
-		}catch (IOException e1){
-			System.out.println ("error: buffer read failed prior to extraction");
-			return -1;
-		}
-		
-		//Extraction stage - get <variable>:<value> out of text document
-		for( int i = 0; (i < MAX) && ( !eof ); i++ ){
-			String varExt, valueExt;
-			
-			varExt = Extract(strLine);
-			valueExt = Extract (varExt);
-			
-			/*
-			 * Add code the extract full var
-			 */
-			
-						
-			/*
-			 * Convert value string to INT
-			 */
-			
-			//var[i] = varExt;
-			//value[i] = valueExt;
-			
-			try{
-				strLine = buf.readLine();	
-				
-				if (strLine == null)
-					eof=true;
-			
-			}catch (IOException e2){
-				System.out.println ("error: buffer read failed during extraction");
-				return -1;
-			}
-		}
-			
-		return 0;
-	}
 	
 }
---
+++ trunk/phoenix/src/testcode/OverlayConfTest.java
@@ -0,0 +1,13 @@
+package testcode;
+
+import java.io.*;
+
+public class OverlayConfTest {
+	
+	public static final int MAX = 5;
+	
+	public static void main (String args[]){
+		
+
+	}
+}
