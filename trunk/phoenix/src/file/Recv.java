edc9f3703434a0dd3540652a5838cfbc906bb57f
conz27 <conz27@97fcad70-2129-0410-99b9-9d13f1ff0228>
2007-03-09 22:06:03 +0000

    Original Send/Recv files from ECE361 website

---
+++ trunk/phoenix/src/file/Recv.java
@@ -0,0 +1,97 @@
+import java.io.*;
+import java.net.*;
+
+/*
+ * Created by: Shadanan Sharma
+ * ECE361 - Simple data transfer tool.
+ * Created on: 22-Dec-2005
+ * E-Mail: shadanan@gmail.com
+ * Web Site: http://www.convergence2000.com
+ */
+
+public class Recv extends Thread {
+	public static final int NOT_STARTED = 0;
+	public static final int RECEIVING = 1;
+	public static final int FAILED = 2;
+	public static final int SUCCEEDED = 3;
+	
+	private ServerSocket ss;
+	private Socket s;
+	private InputStream in; //Input from socket.
+	
+	private int port;
+	private int size;
+	private byte[] data;
+	private int state;
+	private boolean running;
+	
+	public Recv(int port, int size) {
+		this.port = port;
+		this.size = size;
+		this.data = new byte[(int)size];
+		this.state = NOT_STARTED;
+		this.running = true;
+	}
+	
+	public int getRecvState() {
+		return state;
+	}
+	
+	/**
+	 * If state is failed, this will return null.
+	 * @return
+	 */
+	public byte[] getData() {
+		if (state == SUCCEEDED) return data;
+		else return null;
+	}
+	
+	public void close() {
+		running = false;
+		try {
+			if (in != null) in.close();
+		} catch (IOException e) {}
+		try {
+			if (s != null) s.close();
+		} catch (IOException e) {}
+		try {
+			if (ss != null) ss.close();
+		} catch (IOException e) {}
+	}
+	
+	public void run() {
+		state = RECEIVING;
+		try {
+			ss = new ServerSocket(port);
+			s = ss.accept();
+			in = s.getInputStream();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+			state = FAILED;
+			close();
+			return;
+		} catch (IOException e) {
+			e.printStackTrace();
+			state = FAILED;
+			close();
+			return;
+		}
+		
+		int n = 0;
+		while (running) {
+			try {
+				int len = in.read(data, n, data.length-n);
+				n += len;
+				if (n == size) running = false;
+			} catch (IOException e) {
+				e.printStackTrace();
+				state = FAILED;
+				close();
+				return;
+			}
+		}
+		
+		state = SUCCEEDED;
+		close();
+	}
+}
---
+++ trunk/phoenix/src/file/Send.java
@@ -0,0 +1,77 @@
+import java.io.*;
+import java.net.*;
+
+/*
+ * Created by: Shadanan Sharma
+ * Created on: 26-Dec-2005
+ * E-Mail: shadanan@gmail.com
+ * Web Site: http://www.convergence2000.com
+ */
+
+public class Send extends Thread {
+	public static final int NOT_STARTED = 0;
+	public static final int SENDING = 1;
+	public static final int FAILED = 2;
+	public static final int SUCCEEDED = 3;
+	
+	private Socket s;
+	private OutputStream out;
+	
+	private String dest;
+	private int port;
+	private byte[] data;
+	private int state;
+	private boolean running;
+	
+	public Send(String dest, int port, byte[] data) {
+		this.dest = dest;
+		this.port = port;
+		this.data = data;
+		this.state = NOT_STARTED;
+		this.running = true;
+	}
+	
+	public int getSendState() {
+		return state;
+	}
+	
+	public void close() {
+		running = false;
+		if (s != null) try {
+			s.close();
+		} catch (IOException e) {}
+		if (out != null) try {
+			out.close();
+		} catch (IOException e) {}
+	}
+	
+	public void run() {
+		state = SENDING;
+		try {
+			s = new Socket(dest, port);
+			out = s.getOutputStream();
+		} catch (UnknownHostException e) {
+			e.printStackTrace();
+			state = FAILED;
+			return;
+		} catch (IOException e) {
+			e.printStackTrace();
+			state = FAILED;
+			return;
+		}
+		
+		if (running) {
+			try {
+				out.write(data);
+			} catch (IOException e) {
+				e.printStackTrace();
+				state = FAILED;
+				close();
+				return;
+			}
+		}
+		
+		state = SUCCEEDED;
+		close();
+	}
+}
