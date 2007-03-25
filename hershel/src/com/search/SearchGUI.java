2272316715a41d36bfa24e7f8016aeca6f68be4e
jason.bullers <jason.bullers@97fcad70-2129-0410-99b9-9d13f1ff0228>
2007-03-25 14:36:39 +0000

    Nodes display in the Monitor. It seems that the initial node shows up multiple times. Not sure why.
    Changed P2PSettings: auto finding nodes disabled by default.

--- hershel/src/com/search/SearchGUI.java
+++ hershel/src/com/search/SearchGUI.java
@@ -4,20 +4,32 @@ import java.io.IOException;
 import java.net.InetAddress;
 import java.net.InetSocketAddress;
 import java.net.UnknownHostException;
+import java.util.ArrayList;
 import java.util.HashMap;
 
 import com.shadanan.P2PMonitor.IRemote;
 import com.shadanan.P2PMonitor.MonitorService;
 
 public class SearchGUI implements GUI, IRemote {
-
+	private static String initialId =
+		"1234567890123456789012345678901234567890";
 	private MonitorService ms;
 	private NetworkSearchClient client;
 
 	public SearchGUI() throws IOException {
-		client = new NetworkSearchClient("1234567890123456789012345678901234567890", 10000);
-		ms = new MonitorService(10000, this,
+		client = new NetworkSearchClient(initialId, 10000);
+		ms = new MonitorService(10001, this,
 				new InetSocketAddress(InetAddress.getLocalHost(), 10000));
+		client.registerUI(this);
+		client.start();
+		ms.start();
+	}
+
+	public SearchGUI(int port) throws IOException {
+		String randomId = SearchId.getRandomId().toString();
+		client = new NetworkSearchClient(randomId, port);
+		ms = new MonitorService(port + 1, this,
+				new InetSocketAddress(InetAddress.getLocalHost(), port));
 		client.registerUI(this);
 		client.start();
 		ms.start();
@@ -33,8 +45,7 @@ public class SearchGUI implements GUI, IRemote {
 	}
 
 	public void close() {
-		// TODO Auto-generated method stub
-
+		System.out.println("I get called!");
 	}
 
 	public HashMap<String, String> getInfo() {
@@ -44,7 +55,7 @@ public class SearchGUI implements GUI, IRemote {
 
 	public int getLayerCount() {
 		// TODO Auto-generated method stub
-		return 0;
+		return 2;
 	}
 
 	public InetSocketAddress getLocalAddress() {
@@ -56,8 +67,17 @@ public class SearchGUI implements GUI, IRemote {
 	}
 
 	public InetSocketAddress[] getPeers(int layer) {
-		// TODO Auto-generated method stub
-		return null;
+		ArrayList<ArrayList<NodeState>> rt =
+			client.getHandler().routingTable().getRoutingTable();
+		ArrayList<InetSocketAddress> peers =
+			new ArrayList<InetSocketAddress>();
+		for (int i = 0; i < 160; i++) {
+			ArrayList<NodeState> kbucket = rt.get(i);
+			for (NodeState n : kbucket) {
+				peers.add(new InetSocketAddress(n.address, n.port));
+			}
+		}
+		return peers.toArray(new InetSocketAddress[0]);
 	}
 
 	public void message(String text) {
@@ -70,10 +90,6 @@ public class SearchGUI implements GUI, IRemote {
 				ms.println("! Searching error.\n");
 			}
 		}
-	}
-
-	public static void main(String[] args) throws IOException {
-		new SearchGUI();
 	}
 
 }
