db51006fee49362365018eb8e8663a8b53e1dfe5
m.siniavine <m.siniavine@97fcad70-2129-0410-99b9-9d13f1ff0228>
2007-03-27 21:00:38 +0000

    Added forgotten file

---
+++ trunk/hershel/src/com/filetransfer/FileList.java
@@ -0,0 +1,11 @@
+package com.filetransfer;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public interface FileList
+{
+
+    public Piece getPiece(String file, int index) throws FileNotFoundException, IOException;
+
+}
