a144f272e92f7208c777d6827c0c92447fbcbd46
m.siniavine <m.siniavine@97fcad70-2129-0410-99b9-9d13f1ff0228>
2007-03-28 03:23:49 +0000

    Added some networking code

--- hershel/src/test/filetransfer/ServerTests.java
+++ hershel/src/test/filetransfer/ServerTests.java
@@ -1,6 +1,8 @@
 package test.filetransfer;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.io.Writer;
 import java.net.InetSocketAddress;
@@ -23,14 +25,15 @@ public class ServerTests
     private Socket client;
     private MockEventListener mock;
 
-    @Before public void setUp() throws IOException
+    @Before public void setUp() throws IOException, InterruptedException
     {
         mock = new MockEventListener();
         s = new FileTransferServer(10000, mock);
         s.start();
         
+       
         client = new Socket();
-        client.connect(new InetSocketAddress("localhost", 10000));
+        client.connect(new InetSocketAddress("localhost", 10000));       
     }
     
     @After public void tearDown() throws IOException
@@ -53,6 +56,26 @@ public class ServerTests
         
         Assert.assertNotNull(mock.lastSignaled);
         out.close();        
+    }
+    
+    @Test public void sendMessagesByIpAddress() throws InterruptedException, IOException
+    {
+        Thread.sleep(100);
+        Assert.assertEquals(1, s.connectedPeers().size());
+        s.send((InetSocketAddress)s.connectedPeers().keySet().toArray()[0], "hello\r\n");        
+        
+        BufferedReader in = new BufferedReader(new InputStreamReader(client.getInputStream()));
+        Assert.assertEquals("hello", in.readLine());
+    }
+    
+    @Test public void removeDisconnectingPeers() throws InterruptedException, IOException
+    {
+        Thread.sleep(100);
+        Assert.assertEquals(1, s.connectedPeers().size());
+        client.shutdownOutput();
+        client.shutdownInput();
+        Thread.sleep(100);
+        Assert.assertEquals(0, s.connectedPeers().size());
     }
     
     public class MockEventListener implements SocketEventListener
