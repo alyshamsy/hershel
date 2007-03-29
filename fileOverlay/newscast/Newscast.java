3fd31cf741253874fbe4dbbe421ec74de4dd0f40
conz27 <conz27@97fcad70-2129-0410-99b9-9d13f1ff0228>
2007-03-29 03:31:04 +0000

    Newscast class

---
+++ fileOverlay/newscast/Newscast.java
@@ -0,0 +1,12 @@
+package newscast;
+
+import newscast.PartialView;
+
+public class Newscast {
+	public static final int SIZE = 20;
+	
+	private PartialView []view = new PartialView[SIZE];
+	
+	
+	
+}
