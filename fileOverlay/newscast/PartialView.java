11fb941d533c1dbfd3743c88d3100e55c64630e2
conz27 <conz27@97fcad70-2129-0410-99b9-9d13f1ff0228>
2007-03-29 03:05:00 +0000

    PartialView class created.

---
+++ fileOverlay/newscast/PartialView.java
@@ -0,0 +1,46 @@
+package newscast;
+
+import java.io.*;
+import java.net.*;
+
+import java.sql.Timestamp;
+
+public class PartialView {
+	InetAddress addr;
+	Timestamp time;
+	
+	PartialView(){
+		this.addr = null;
+		this.time = null;
+	}
+	
+	PartialView (InetAddress addr, Timestamp time){
+		this.addr = addr;
+		this.time = time;
+	}
+	
+	void setAddr(InetAddress addr){
+		this.addr = addr;
+	}
+	
+	InetAddress getAddr(){
+		return this.addr;
+	}
+	
+	Timestamp getTime(){
+		return this.time;
+	}
+	
+	void setTime(long time){
+		this.time.setTime(time);
+	}
+	
+	void updateTime (Timestamp temp){
+		long newTime = temp.getTime();
+		long oldTime = time.getTime();
+		long updateTime = newTime - oldTime;
+		
+		time.setTime(updateTime);
+	}
+	
+}
